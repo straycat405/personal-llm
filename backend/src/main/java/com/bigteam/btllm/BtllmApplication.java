42be0cfffbee855fe42b5d3a75c9d72b0072ac7e
DongJin Park <straycat405@gmail.com>
2026-05-20 11:19:03 +0900

    init: monorepo 구조 설정 (backend / frontend) ;

---
+++ backend/src/main/java/com/bigteam/btllm/BtllmApplication.java
@@ -0,0 +1,13 @@
+package com.bigteam.btllm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtllmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtllmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bigteam/btllm/BtllmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigteam.btllm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtllmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
